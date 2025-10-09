c354175c7eaeadf711da90fe81e2389e79a0bb11
karizmagreen18 <Brooklynkay120@gmail.com>
2025-10-08 21:30:16 -0400

    not done with this assignment

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,11 +1,58 @@
-const measureRain = () => {
+const measureRain = (inches) => {
+  if (inches === 0 ) {
+    return 'drought'
+  } else if (inches < 2) {
+    return 'dry'
+  } else if (inches < 4) {
+    return 'average'
+  } else if (inches < 6) {
+    return 'rainy'
+  } else {
+    return 'flood'
+  }
+ 
 };
+console.log(measureRain(7));
 
-const happyBirthdayPet = () => {
-};
+const happyBirthdayPet = (breed, age) => {
+  if (breed === 'snake') {
+    return 'Hiss hiss!'
+  } else if (breed === 'cat' && age < 5 ) {
+    return 'Mew mew!'
+  } else if (breed === 'cat' && age >= 5 ) {
+    return 'Meow meow!'
+  } else if (breed === 'dog' && age < 5 ) {
+    return 'Arf arf!'
+  } else if (breed === 'dog' && age >= 5 && age < 10 ) {
+    return 'Woof woof!'
+  } else if (breed === 'dog' && age >= 10 ) {
+    return 'Boof!'
+  } else {
+    return 'Happy birthday!'
+  }
 
-const funTypes = () => {
 };
+console.log(happyBirthdayPet('dog'))
+
+const funTypes = (jsType) => {
+ if (jsType === 'string') {
+  return "That's just some text."
+ } else if (jsType === 'number') {
+  return "That's a good number."
+ } else if (jsType === 'boolean') {
+  return "To bool, or not to bool?"
+ } else if (jsType === 'undefined') {
+  return "Nothing, but I didn't set that."
+ } else if (jsType === 'null') {
+  return "Nothing, and I did set that."
+ } else if (jsType === 'object') {
+  return "Anybody got the key?"
+ } else if (jsType === 'array') {
+  return "I order you to be indexed."
+ } else if (jsType === 'nan') {
+  return "Well, now you're just showing off."
+ }
+}; console.log(funTypes('boolean'))
 
 const rounder = () => {
 };
